88b8b909e1e6edc4f9bf92553ff8ffb7f0ae9513
Mahmoud Mansour <mahmodlte@gmail.com>
2021-07-10 10:53:39 +0300

    Implemented data fetching for cards on button click to draw the cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,29 @@
-const Card = (props) => {
-  return <div className="card"></div>;
+const Card = ({ cardValues }) => {
+  // console.log(typeof cardValues.cards);
+  return (
+    <div>
+      {cardValues
+        ? cardValues.map((values) => {
+            return (
+              <div className="cardsContainer">
+                <div className="card">
+                  <img src={values.image} alt={values.suit} />
+                </div>
+              </div>
+            );
+          })
+        : console.warn("there is no data")}
+    </div>
+  );
 };
+export default Card;
+// {
+//  {deckDraw
+//             ? deckDraw.cards.map((cardInfo) => {
+//                 return (
+
+//                 );
+//               })
+//             : console.warn("there is no data on deckDraw")}
+
+// }
--- src/containers/Mainpage.jsx
+++ src/containers/Mainpage.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import "../App.css";
-
+import Card from "../components/Card/Card";
 const MainPage = () => {
   const BASE_URL =
     "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
   const [deckId, setDeckId] = useState();
+  const [deckDraw, setDeckDraw] = useState();
   useEffect(() => {
     const gameStart = async () => {
       const res = await fetch(BASE_URL);
@@ -18,9 +19,9 @@ const MainPage = () => {
       `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
     );
     const deckData = await res.json();
-    console.log(deckData);
+    setDeckDraw(deckData.cards);
   };
-
+  console.log(deckDraw);
   return (
     <div className="container">
       <h1 className="heading">Deck of 52 cards</h1>
@@ -28,9 +29,21 @@ const MainPage = () => {
         <button className="button" onClick={drawGame}>
           Deal
         </button>
+        <Card cardValues={deckDraw} />
       </div>
     </div>
   );
 };
 
 export default MainPage;
+// {deckDraw
+//   ? deckDraw.cards.map((cardInfo) => {
+//       return (
+//         <div className="cardsContainer">
+//           <div className="card">
+//             <img src={cardInfo.image} alt={cardInfo.suit} />
+//           </div>
+//         </div>
+//       );
+//     })
+//   : console.warn("there is no data on deckDraw")}
